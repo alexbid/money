60e3cbeacfdc8b2f3f4bced70067179ea0077a40
alexbid <bidault@hotmail.fr>
2020-03-22 14:21:10 +0100

    update scripts

--- money2.py
+++ money2.py
@@ -70,10 +70,10 @@ class pdf:
                 if len(val) == 4:
                     date_stock=datetime.datetime(2020, self.getMonth(val[1]), int(val[0]))
                     date_valeur=datetime.datetime(2020, self.getMonth(val[3]), int(val[2]))
-                    return [date_stock.strftime("%m/%d/%Y"), date_valeur.strftime("%d/%m/%Y")]
+                    return [date_stock.strftime("%d/%m/%Y"), date_valeur.strftime("%d/%m/%Y")]
                 elif len(val) == 2:
                     date_stock=datetime.datetime(2020, self.getMonth(val[1]), int(val[0]))
-                    return [date_stock.strftime("%m/%d/%Y"), date_stock.strftime("%d/%m/%Y")]
+                    return [date_stock.strftime("%d/%m/%Y"), date_stock.strftime("%d/%m/%Y")]
                 elif str(lyne) == 'nan': return ['NaN', 'NaN']
                 elif len(val) > 4: return ['NaN', 'NaN']
                 else:
